dd9a4e758b23ee19f7d16d7289394d93342b0644
Your name <Your@emailaddres.com>
2026-02-24 14:50:32 +0100

    Syntax error in tests verbeterd

--- src/process/process.py
+++ src/process/process.py
@@ -1,11 +1,11 @@
-def addition(a, b)
+def addition(a, b):
     output = a + b
     return output
 
-def subtraction(a, b)
+def subtraction(a, b):
     output = a - b
     return output
 
-def multiplication(a, b)
+def multiplication(a, b):
     output = a * b
     return output
